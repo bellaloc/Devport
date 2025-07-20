1f7b2ad41cdbeeae37110243cc6e7ebb78380dd7
Christa Lococo <montecitocandleco@gmail.com>
2025-07-19 18:09:58 -0700

    Remove nested repo folder from tracking

--- Devport/creavolio/src/components/common/projects/ProjectTag.tsx
+++
@@ -1,9 +0,0 @@
-const ProjectTag = ({ tag }: { tag: string }) => {
-  return (
-    <span className="rounded-full bg-[var(--bg-primary-inverse)] text-[var(--text-primary-inverse)] text-sm text-nowrap font-[300] py-2 px-4">
-      {tag}
-    </span>
-  );
-};
-
-export default ProjectTag;
--- Devport/creavolio/src/data/app.ts
+++
@@ -1,8 +0,0 @@
-// ==================[ App Info ]================== //
-export const app_info = {
-  title: "Personal Portfolio / cv",
-
-  description:
-    "Experienced web developer specializing in front-end and full-stack development using JavaScript, Node.js, React, Next.js, and Tailwind CSS. I create responsive, SEO-friendly websites and applications tailored to your business needs. Contact me for high-quality web development services.",
-} as const;
-//
